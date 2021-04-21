45374a8c30dad40e2f72bcb5df7914009d762059
Bibekspk <bibeksp7@gmail.com>
2021-04-21 22:07:05 +0545

    final commit

--- Frontend/lib/src/components/body.dart
+++ Frontend/lib/src/components/body.dart
@@ -37,130 +37,125 @@ class _State extends State<Body> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return SingleChildScrollView(
-        child: Column(children: <Widget>[
+    return Column(children: <Widget>[
       Container(
-        height: size.height * 0.22, //covers 20 percent
+        height: size.height * 0.24, //covers 20 percent
         decoration: BoxDecoration(
             color: Colors.green,
             borderRadius: BorderRadius.only(
               bottomLeft: Radius.circular(30),
               bottomRight: Radius.circular(30),
             )),
-        child: SingleChildScrollView(
-          child: Stack(
-            children: <Widget>[
-              Container(
-                  margin: EdgeInsets.fromLTRB(15, 0, 0, 0),
-                  child: Row(
-                    children: <Widget>[
-                      Text(
-                        "Welcome, ",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 19),
+        child: Stack(
+          children: <Widget>[
+            Container(
+                margin: EdgeInsets.fromLTRB(15, 0, 0, 0),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      "Welcome, ",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 19),
+                    ),
+                    FutureBuilder(
+                        future: FlutterSession().get('name'),
+                        builder: (context, snapshot) {
+                          return Text(
+                              snapshot.hasData ? snapshot.data : "Loading",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 19));
+                        }),
+                    Spacer(),
+                    IconButton(
+                      padding: EdgeInsets.fromLTRB(0, 0, 35, 0),
+                      icon: Icon(
+                        Icons.person_sharp,
+                        size: 35,
                       ),
-                      FutureBuilder(
-                          future: FlutterSession().get('name'),
-                          builder: (context, snapshot) {
-                            return Text(
-                                snapshot.hasData ? snapshot.data : "Loading",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 19));
-                          }),
-                      Spacer(),
-                      IconButton(
-                        padding: EdgeInsets.fromLTRB(0, 0, 35, 0),
-                        icon: Icon(
-                          Icons.person_sharp,
-                          size: 35,
-                        ),
-                        onPressed: () {},
-                      ),
-                    ],
-                  )),
-              Positioned(
-                child: Container(
-                  alignment: Alignment.center,
-                  margin: EdgeInsets.fromLTRB(15, 40, 10, 0),
-                  height: 45,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                          offset: Offset(0, 10),
-                          blurRadius: 50,
-                          color: Colors.white.withOpacity(0.23),
-                        )
-                      ]),
-                  child: TextFormField(
-                    controller: search,
-                    style: TextStyle(fontSize: 21),
-                    decoration: InputDecoration(
-                        // labelText: "   Search",
-                        hintText: "Search",
-                        contentPadding: EdgeInsets.fromLTRB(7, 7.5, 0, 0),
-                        hintStyle: TextStyle(
-                            color: Colors.black.withOpacity(0.5),
-                            fontSize: 22.5,
-                            fontWeight: FontWeight.bold),
-                        enabledBorder: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        suffixIcon: IconButton(
-                          icon: Icon(Icons.search),
-                          onPressed: () {
-                            if (widget.startPrice != null ||
-                                widget.endPrice != null) {
-                              print("Search " + search.text);
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => SearchedRooms(
-                                            search: search.text,
-                                            startPrice: startRange,
-                                            endPrice: endRange,
-                                          )));
-                            } else {
-                              EasyLoading.showInfo(
-                                  "Provide Range of Price for better search",
-                                  maskType: EasyLoadingMaskType.black);
-                            }
-                          },
-                        )),
-                  ),
+                      onPressed: () {},
+                    ),
+                  ],
+                )),
+            Positioned(
+              child: Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.fromLTRB(15, 40, 10, 0),
+                height: 45,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: Colors.white.withOpacity(0.23),
+                      )
+                    ]),
+                child: TextFormField(
+                  controller: search,
+                  style: TextStyle(fontSize: 21),
+                  decoration: InputDecoration(
+                      // labelText: "   Search",
+                      hintText: "Search",
+                      contentPadding: EdgeInsets.fromLTRB(7, 7.5, 0, 0),
+                      hintStyle: TextStyle(
+                          color: Colors.black.withOpacity(0.5),
+                          fontSize: 22.5,
+                          fontWeight: FontWeight.bold),
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.search),
+                        onPressed: () {
+                          if (widget.startPrice != null ||
+                              widget.endPrice != null) {
+                            print("Search " + search.text);
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SearchedRooms(
+                                          search: search.text,
+                                          startPrice: startRange,
+                                          endPrice: endRange,
+                                        )));
+                          } else {
+                            EasyLoading.showInfo(
+                                "Provide Range of Price for better search",
+                                maskType: EasyLoadingMaskType.black);
+                          }
+                        },
+                      )),
                 ),
               ),
-              Container(
-                  height: 35,
-                  // width: 100,
-                  padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                  margin: EdgeInsets.fromLTRB(240, 90, 5, 0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  child: RaisedButton(
-                      textColor: Colors.black,
-                      color: Colors.blue,
-                      child: Text(
-                        'Filter',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => FilterPage()));
-                      })),
-            ],
-          ),
+            ),
+            Container(
+                height: 35,
+                // width: 100,
+                padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                margin: EdgeInsets.fromLTRB(240, 90, 5, 0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: RaisedButton(
+                    textColor: Colors.black,
+                    color: Colors.blue,
+                    child: Text(
+                      'Filter',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => FilterPage()));
+                    })),
+          ],
         ),
       ),
       Container(
-        child: SingleChildScrollView(
-          child: PropertyCarousel(),
-        ),
+        child: PropertyCarousel(),
       ),
-    ]));
+    ]);
     // 'localhost:5000/v2/getimg/1617095763881.jpg'
     // http://10.0.2.2:5000/v2/getimg/1617095763881.jpg/
   }
